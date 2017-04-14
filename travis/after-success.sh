7c15463294f8d41a96ffeef29bfd8c586127c860
Danilo Horta <danilo.horta@gmail.com>
2017-04-14 16:11:26 +0100

    || true

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -5,12 +5,12 @@ if ! [ -z ${DOCKER_IMAGE+x} ]; then
     docker run -e PYPI_PASSWORD=${PYPI_PASSWORD} --rm -v `pwd`:/io $DOCKER_IMAGE /bin/bash
     pip install twine -q
     twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/${PKG_NAME}*.whl \
-          -u dhorta -p ${PYPI_PASSWORD}
+          -u dhorta -p ${PYPI_PASSWORD} || true
 else
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         source ~/.venv/bin/activate
         pip install twine -q
         twine upload ${TRAVIS_BUILD_DIR}/dist/${PKG_NAME}*.whl \
-              -u dhorta -p ${PYPI_PASSWORD}
+              -u dhorta -p ${PYPI_PASSWORD} || true
     fi
 fi
