9b9d76d686fcb54211779b5025d86346edf97b42
Danilo Horta <danilo.horta@gmail.com>
2017-04-14 16:00:02 +0100

    Less ls

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -3,14 +3,11 @@ set -e -x
 
 if ! [ -z ${DOCKER_IMAGE+x} ]; then
     docker run -e PYPI_PASSWORD=${PYPI_PASSWORD} --rm -v `pwd`:/io $DOCKER_IMAGE /bin/bash
-    ls
-    ls wheelhouse/
     pip install twine -q
     twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/${PKG_NAME}*.whl \
           -u dhorta -p ${PYPI_PASSWORD}
 else
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-        ls dist
         source ~/.venv/bin/activate
         pip install twine -q
         twine upload ${TRAVIS_BUILD_DIR}/dist/${PKG_NAME}*.whl \
