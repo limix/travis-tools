66afae5eac0e4462ff20366d8df661589f0b5c9b
Danilo Horta <danilo.horta@gmail.com>
2017-04-14 15:41:11 +0100

    Chmod everyone

--- get-travis-tools.sh
+++ get-travis-tools.sh
@@ -7,11 +7,19 @@ URL_PREFIX=https://raw.githubusercontent.com/limix/travis-tools/master/travis/
 
 pushd travis
 curl -O ${URL_PREFIX}/after-success.sh
+chmod +x after-success.sh
 curl -O ${URL_PREFIX}/before-install.sh
+chmod +x before-install.sh
 curl -O ${URL_PREFIX}/build-wheels.sh
+chmod +x build-wheels.sh
 curl -O ${URL_PREFIX}/deploy-wheels.sh
+chmod +x deploy-wheels.sh
 curl -O ${URL_PREFIX}/install-pandoc.sh
+chmod +x install-pandoc.sh
 curl -O ${URL_PREFIX}/prepare-for-osx.sh
+chmod +x prepare-for-osx.sh
 curl -O ${URL_PREFIX}/script.sh
+chmod +x script.sh
 curl -O ${URL_PREFIX}/test.sh
+chmod +x test.sh
 popd
