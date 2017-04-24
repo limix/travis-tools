58788eb076aa93b7d12a5c34c87432f26d239b86
Danilo Horta <danilo.horta@gmail.com>
2017-04-24 10:17:37 +0100

    Only test on p36

--- travis/test-before-install-manylinux.sh
+++ travis/test-before-install-manylinux.sh
@@ -19,14 +19,12 @@ fi
 
 
 for PYBIN in /opt/python/*/bin; do
-    if [[ $PYBIN == *"p26"* ]] || [[ $PYBIN == *"p33"* ]] || \
-       [[ $PYBIN == *"p34"* ]]; then
-        continue
+    if [[ $PYBIN == *"p36"* ]]; then
+        "${PYBIN}/pip" install setuptools --upgrade -q
+        "${PYBIN}/pip" install cython --upgrade -q
+        eval "${PYBIN}/pip" install "${PY_DEPS}" -q
+        pushd /io
+        "${PYBIN}/python" setup.py test
+        popd
     fi
-    "${PYBIN}/pip" install setuptools --upgrade -q
-    "${PYBIN}/pip" install cython --upgrade -q
-    eval "${PYBIN}/pip" install "${PY_DEPS}" -q
-    pushd /io
-    "${PYBIN}/python" setup.py test
-    popd
 done
