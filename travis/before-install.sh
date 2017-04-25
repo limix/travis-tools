9eb625212a9cd4545ec2b4a8cae0cbadaa80a0d8
Danilo Horta <danilo.horta@gmail.com>
2017-04-25 16:43:17 +0100

    Removed NUMBA

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -5,15 +5,9 @@ if [ -z ${DOCKER_IMAGE+x} ]; then
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         travis/prepare-for-osx.sh
     else
-        if [ "${NUMBA}" == "true" ]; then
-            travis/install-clang38.sh
-            travis/install-llvmlite.sh
-        fi
     fi
 
     pip install wheel setuptools cython numpy --upgrade -q
-
-    # eval pip install "${PY_DEPS}" -q
 
     travis/install-pandoc.sh python
 
--- travis/test-before-install.sh
+++ travis/test-before-install.sh
@@ -5,7 +5,6 @@ if [ -z ${DOCKER_IMAGE+x} ]; then
     if [[ $TRAVIS_OS_NAME != 'linux' ]]; then
         source ~/.venv/bin/activate
     fi
-    # eval pip install "${PY_DEPS}" -q
     python setup.py test
 else
     docker run -e PKG_NAME=${PKG_NAME} -e PY_DEPS="${PY_DEPS}" \
