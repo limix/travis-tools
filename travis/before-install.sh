a663a3bc571edc1b8bfb636372061c8693be4809
Danilo Horta <danilo.horta@gmail.com>
2017-04-26 09:57:45 +0100

    Pydeps

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e -x
 
+pydeps="$1"
+
 if [ -z ${DOCKER_IMAGE+x} ]; then
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         source travis/prepare-for-osx.sh
     fi
 
     pip install wheel setuptools cython numpy --upgrade -q
+
+    if ! [ -z ${pydeps} ]; then
+        eval pip install ${pydeps} --upgrade -q
+    fi
 
     source travis/install-pandoc.sh
 
