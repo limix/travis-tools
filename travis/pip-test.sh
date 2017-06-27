0d21c1a91c1af62a4c3ec24a8f90e801bfca4161
Danilo Horta <danilo.horta@gmail.com>
2017-06-27 20:04:13 +0100

    pyptest-8

--- travis/pip-test.sh
+++ travis/pip-test.sh
@@ -3,7 +3,7 @@ set -e -x
 
 BIN="$1"
 
-"${BIN}pip" install pytest -q
+"${BIN}pip" install pytest pytest-pep8 -q
 
 pushd /
 "${BIN}python" -c "import sys; import ${PKG_NAME}; sys.exit(${PKG_NAME}.test())"
