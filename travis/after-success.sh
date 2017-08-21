47a2ac73876cbba3aeba2aa19667f24433afdfee
Danilo Horta <danilo.horta@gmail.com>
2017-08-21 10:14:27 +0100

    PRJ to PKG after success, debug messages

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -2,16 +2,19 @@
 set -e
 
 echo "
-333333333333333333333333333333333333333333333333333333333333333333333333333333
-333333333333333333333333333333 AFTER SUCCESS BEGIN 333333333333333333333333333
-333333333333333333333333333333333333333333333333333333333333333333333333333333
+##############################################################################
+############################## AFTER SUCCESS BEGIN ###########################
+##############################################################################
 "
 
 DOCK=true && [[ -z "${DOCKER_IMAGE+x}" ]] && DOCK=false || true
 
 set -x
+
+echo "(AFTER_SUCCESS) Defining names."
 source travis/util/define-names.sh
 
+echo "(AFTER_SUCCESS) Shall we deploy WHEEL?"
 if ! [ -z "${DONT_RELEASE_WHEEL+x}" ]; then
     if [ "${DONT_RELEASE_WHEEL}" == "true" ]; then
         exit 0
@@ -19,24 +22,28 @@ if ! [ -z "${DONT_RELEASE_WHEEL+x}" ]; then
 fi
 
 if ! [ -z ${TRAVIS_TAG} ]; then
+    echo "(AFTER_SUCCESS) Tagged commit. Deploying stage."
 
     if [ "$DOCK" = true ]; then
 
+        echo "(AFTER_SUCCESS) Run docker image and upload ${PKG_NAME} wheels."
         CMD=/bin/bash
         docker run --env-file ~/env.list -e PYPI_PASSWORD=${PYPI_PASSWORD}  \
             --rm -v `pwd`:/io $DOCKER_IMAGE $CMD
 
         pip install twine --upgrade -q
 
-        twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/${PRJ_NAME}*.whl \
+        twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/${PKG_NAME}*.whl \
             -u dhorta -p ${PYPI_PASSWORD} || true
     else
         if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
+            
+            echo "(AFTER_SUCCESS) Activate Python environment and upload ${PKG_NAME} wheels."
             source ~/.venv/bin/activate
             pip install twine --upgrade -q
 
-            twine upload ${TRAVIS_BUILD_DIR}/dist/${PRJ_NAME}*.whl \
+            twine upload ${TRAVIS_BUILD_DIR}/dist/${PKG_NAME}*.whl \
                 -u dhorta -p ${PYPI_PASSWORD} || true
         fi
     fi
@@ -45,7 +52,7 @@ fi
 set +x
 
 echo "
-333333333333333333333333333333333333333333333333333333333333333333333333333333
-3333333333333333333333333333333 AFTER SUCCESS END 3333333333333333333333333333
-333333333333333333333333333333333333333333333333333333333333333333333333333333
+##############################################################################
+############################### AFTER SUCCESS END ############################
+##############################################################################
 "
