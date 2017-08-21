56e77e85082237366605fc5ee27abfb0d062315c
Danilo Horta <danilo.horta@gmail.com>
2017-08-21 09:25:51 +0100

    No file creation then

--- travis/util/pip-test.sh
+++ travis/util/pip-test.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 set -e
-set -x
 
 pushd /
-pwd
-touch test_this.py
-echo "
-import sys
-import ${PKG_NAME}
-code = ${PKG_NAME}.test()
+
+$1/python -c "
+import sys;
+import ${PKG_NAME};
+code = ${PKG_NAME}.test();
 sys.exit(code)
-" > test_this.py
-
-ls
-chmod 777 test_this.py
-chmod +x test_this.py
-$1/python test_this.py
-
-rm test_this.py
+"
 
 popd
