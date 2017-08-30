ac83302f49a52337680e30d735eb862e671be6e2
Danilo Horta <danilo.horta@gmail.com>
2017-08-30 13:46:56 +0100

    remove pyarrow

--- travis/util/install-py-deps.sh
+++ travis/util/install-py-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$1/pip install wheel setuptools rstcheck pyarrow --upgrade -q
+$1/pip install wheel setuptools rstcheck --upgrade -q
 $1/pip install cython --upgrade -q
 $1/pip install pyarrow --upgrade -q
