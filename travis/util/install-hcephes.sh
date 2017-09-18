d7c72f081a257c8f257e74ea568c7cf3a6d707d5
Danilo Horta <danilo.horta@gmail.com>
2017-09-18 18:32:21 +0100

    source <(curl -s https://raw.githubusercontent.com/limix/hcephes/master/install)

--- travis/util/install-hcephes.sh
+++ travis/util/install-hcephes.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
 set -e -x
 
-pushd .
-rm -rf hcephes-build || true
-wget https://github.com/limix/hcephes/archive/0.1.5.tar.gz
-tar xzf 0.1.5.tar.gz
-mv hcephes-0.1.5 hcephes-build
-cd hcephes-build
-mkdir build
-cd build
-cmake ..
-make
-make test
-
-set +e
-sudo &> /dev/null
-err="$?"
-if [[ "$err" == "1" ]]; then
-    SUDO="sudo"
-else
-    SUDO=""
-fi
-set -e
-
-eval "$SUDO" make install
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    eval "$SUDO" ldconfig
-fi
-popd
-rm -rf hcephes-build || true
+source <(curl -s https://raw.githubusercontent.com/limix/hcephes/master/install)
