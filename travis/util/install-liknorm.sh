46458b10debd33f677104723107f31e8990022d1
Danilo Horta <danilo.horta@gmail.com>
2017-09-19 14:10:29 +0100

    bash <(curl -fsSL https://raw.githubusercontent.com/limix/liknorm/master/install)

--- travis/util/install-liknorm.sh
+++ travis/util/install-liknorm.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
-set -e -x
 
-pushd .
-rm -rf liknorm-build || true
-git clone https://github.com/limix/liknorm.git liknorm-build
-cd liknorm-build
-mkdir build
-cd build
-cmake ..
-make
-make test
-
-set +e
-sudo &> /dev/null
-err="$?"
-if [[ "$err" == "1" ]]; then
-    SUDO="sudo"
-else
-    SUDO=""
-fi
-set -e
-
-eval "$SUDO" make install
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    eval "$SUDO" ldconfig
-fi
-popd
-rm -rf liknorm-build || true
+bash <(curl -fsSL https://raw.githubusercontent.com/limix/liknorm/master/install)
