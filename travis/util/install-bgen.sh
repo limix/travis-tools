a7b6801433517d97cc32791de2a30837abd845c9
Danilo Horta <danilo.horta@gmail.com>
2017-09-19 16:49:49 +0100

    bgen install

--- travis/util/install-bgen.sh
+++ travis/util/install-bgen.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
-set -e -x
 
-pushd .
-rm -rf bgen-build || true
-git clone https://github.com/limix/bgen.git bgen-build
-cd bgen-build
-mkdir build
-cd build
-cmake ..
-make
-make test
-
-set +e
-sudo &> /dev/null
-err="$?"
-if [[ "$err" == "1" ]]; then
-    SUDO="sudo"
-else
-    SUDO=""
-fi
-set -e
-
-eval "$SUDO" make install
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    eval "$SUDO" ldconfig
-fi
-popd
-rm -rf bgen-build || true
+bash <(curl -fsSL https://raw.githubusercontent.com/limix/bgen/master/install)
